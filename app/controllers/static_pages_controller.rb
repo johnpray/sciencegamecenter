d2bb91065592838225183e117f2d1f3abe5f53f2
John Pray <louiegeetoo@gmail.com>
2012-04-04 13:05:29 -0400

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def privacy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SciencegamereviewsOrg::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  get "static_pages/privacy"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
