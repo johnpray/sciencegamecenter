20d9eb99cb6a9e484939843c2884d4f336e2540d
John Pray <louiegeetoo@gmail.com>
2012-04-05 12:33:23 -0400

    Make flashes dismissable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-dropdown
+//= require bootstrap-alert
 //= require_tree .
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,19 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
