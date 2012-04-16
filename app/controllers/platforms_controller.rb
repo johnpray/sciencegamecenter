9a9f6b865d99e550864cf75a0be8496647fbda79
John Pray <louiegeetoo@gmail.com>
2012-04-16 18:14:45 -0400

    .

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -2,7 +2,7 @@ class PlatformsController < ApplicationController
   def index
   	if Rails.env.production?
 			@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms')
-				.select('DISTINCT ON tag_id *')
+				.select('DISTINCT ON (tag_id) *')
   	else
   		@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms').group(:tag_id)
   	end
