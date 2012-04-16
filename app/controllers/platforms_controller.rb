d36c030d8d0880bd11a9743af6917aa72eba77e2
John Pray <louiegeetoo@gmail.com>
2012-04-16 18:01:54 -0400

    Fix duplicates showing up on platforms and subjects lists

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,5 +1,5 @@
 class PlatformsController < ApplicationController
   def index
-  	@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms')
+  	@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms').group(:tag_id)
   end
 end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,5 @@
 class SubjectsController < ApplicationController
   def index
-  	@taggings = ActsAsTaggableOn::Tagging.where(context: 'subjects')
+  	@taggings = ActsAsTaggableOn::Tagging.where(context: 'subjects').group(:tag_id)
   end
 end
