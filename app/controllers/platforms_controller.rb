389ddb42265ee0c96ea14f7afd74b8b98af7968e
John Pray <louiegeetoo@gmail.com>
2012-04-16 18:11:29 -0400

    Attempt pg-specific dupes fix

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,5 +1,10 @@
 class PlatformsController < ApplicationController
   def index
-  	@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms').group(:tag_id)
+  	if Rails.env.production?
+			@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms')
+				.select('DISTINCT ON tag_id *')
+  	else
+  		@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms').group(:tag_id)
+  	end
   end
 end
