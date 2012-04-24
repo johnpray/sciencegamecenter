e16312bcbe50fc8e36a9263a396d7ec5ab4d97c2
John Pray <louiegeetoo@gmail.com>
2012-04-24 13:23:09 -0400

    Add commented forcessl to comments_controller, as it should have been there to begin with

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 
+  #force_ssl
 	before_filter :signed_in_user,  only: [:create, :edit, :update, :destroy]
 	before_filter :correct_user,    only: [:edit, :update, :destroy]
 
