1f8b39811293a2fce99fd1ed3c8833076fd2a7a7
John Pray <louiegeetoo@gmail.com>
2012-04-24 16:03:37 -0400

    Fix platforms and subjects queries for staging

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,10 +1,10 @@
 class PlatformsController < ApplicationController
   def index
-  	if Rails.env.production?
-			@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms')
-				.select('DISTINCT ON (tag_id) *')
-  	else
+  	if Rails.env.development?
   		@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms').group(:tag_id)
+  	else
+  		@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms')
+				.select('DISTINCT ON (tag_id) *')
   	end
   end
 end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,10 +1,10 @@
 class SubjectsController < ApplicationController
   def index
-  	if Rails.env.production?
-			@taggings = ActsAsTaggableOn::Tagging.where(context: 'subjects')
-				.select('DISTINCT ON (tag_id) *')
+  	if Rails.env.development?
+			@taggings = ActsAsTaggableOn::Tagging.where(context: 'subjects').group(:tag_id)
   	else
-  		@taggings = ActsAsTaggableOn::Tagging.where(context: 'subjects').group(:tag_id)
+  		@taggings = ActsAsTaggableOn::Tagging.where(context: 'subjects')
+				.select('DISTINCT ON (tag_id) *')
   	end
   end
 end
