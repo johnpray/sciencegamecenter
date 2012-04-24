3ddb1ce4db499cc718ef6b5eb83a05a972a6100f
John Pray <louiegeetoo@gmail.com>
2012-04-24 13:47:37 -0400

    Prepare configs for staging env

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,7 @@
-if Rails.env == "production"
+if Rails.env == "development"
+	# get credentials from YML file
+  S3_CREDENTIALS = Rails.root.join("config/s3.yml")
+else
   # set credentials from ENV hash
   S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => ENV['S3_BUCKET']}
-else
-  # get credentials from YML file
-  S3_CREDENTIALS = Rails.root.join("config/s3.yml")
 end
