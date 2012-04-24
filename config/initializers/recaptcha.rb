bd7b2d90477e74e0113619bb81edc4ce89102f72
John Pray <louiegeetoo@gmail.com>
2012-04-24 13:52:36 -0400

    Fix recaptcha config for staging

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,13 +1,13 @@
-if Rails.env == "production"
+if Rails.env == "development"
+	# get credentials from YML file
+  Recaptcha.configure do |config|
+	  config.public_key  = Rails.root.join("config/recaptcha_public.txt").read
+	  config.private_key = Rails.root.join("config/recaptcha_private.txt").read
+	end
+else
   # set credentials from ENV hash
   Recaptcha.configure do |config|
 	  config.public_key  = ENV['RECAPTCHA_PUBLIC']
 	  config.private_key = ENV['RECAPTCHA_PRIVATE']
-	end
-else
-  # get credentials from YML file
-  Recaptcha.configure do |config|
-	  config.public_key  = Rails.root.join("config/recaptcha_public.txt").read
-	  config.private_key = Rails.root.join("config/recaptcha_private.txt").read
 	end
 end
