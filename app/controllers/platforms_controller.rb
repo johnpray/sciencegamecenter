f0133ee21fde756543d86526757ab36fbf117065
John Pray <louiegeetoo@gmail.com>
2012-04-25 19:10:41 -0400

    Order platforms by name

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,10 +1,11 @@
 class PlatformsController < ApplicationController
   def index
-  	if Rails.env.development?
-  		@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms').group(:tag_id)
-  	else
-  		@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms')
-				.select('DISTINCT ON (tag_id) *')
-  	end
+  	#if Rails.env.development?
+  		#@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms').group(:tag_id)
+  		@taggings = Game.tag_counts_on('platforms').order('name ASC')
+  	#else
+  	#	@taggings = ActsAsTaggableOn::Tagging.where(context: 'platforms')
+		#		.select('DISTINCT ON (tag_id) *')
+  	#end
   end
 end
