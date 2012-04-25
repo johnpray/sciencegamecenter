9b35e2ce2b82b1bd439b6436fe7356afb61d50e0
John Pray <louiegeetoo@gmail.com>
2012-04-25 15:06:52 -0400

    Add confirmable tracked links helper. Improve about text spacing on home.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,4 +51,28 @@ module ApplicationHelper
       "<a #{href_attr}#{tag_options} onClick=\"recordOutboundLink(this, 'Tracked Links', '#{href || ERB::Util.html_escape(url)}');return false;\">#{ERB::Util.html_escape(name || url)}</a>".html_safe
     end
   end
+
+  # Creates a link that gets tracked in Google Analytics
+  def confirmed_tracked_link_to(*args, &block)
+    if block_given?
+      options      = args.first || {}
+      html_options = args.second
+      link_to(capture(&block), options, html_options)
+    else
+      name         = args[0]
+      options      = args[1] || {}
+      html_options = args[2]
+
+      html_options = convert_options_to_data_attributes(options, html_options)
+      url = url_for(options)
+
+      href = html_options['href']
+      tag_options = tag_options(html_options)
+
+      message = "You\\'re about to leave the Science Game Center to visit one of our generous sponsors. Check them out! We hope to see you again soon."
+
+      href_attr = "href=\"#{ERB::Util.html_escape(url)}\"" unless href
+      "<a #{href_attr}#{tag_options} onClick=\"if(confirm('#{message}')){recordOutboundLink(this, 'Tracked Links', '#{href || ERB::Util.html_escape(url)}');}return false;\">#{ERB::Util.html_escape(name || url)}</a>".html_safe
+    end
+  end
 end
