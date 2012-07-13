856b4cbfcfab84b655470ae5e614456ea5cbbc7a
John Pray <louiegeetoo@gmail.com>
2012-07-13 16:03:53 -0400

    Order comments to show oldest first. Update rating wording in review mailer. Update review edit form styling to match new review form.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   attr_accessible :commentable_id, :commentable_type, :content, :status
 
-  default_scope order: 'created_at DESC'
+  default_scope order: 'created_at ASC'
 
   belongs_to :commentable, polymorphic: true
   belongs_to :user
