f04aceb9f43933868618dcc8233b671b727044ee
John Pray <louiegeetoo@gmail.com>
2013-01-11 17:25:37 -0500

    Add new categ types to game info (shows on index and show)

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -12,6 +12,24 @@ module GamesHelper
 		end
 	end
 
+	def intended_for_links_list(game = @game)
+		if game && game.intended_fors
+			game.intended_fors.order('name ASC').collect {|t| link_to(capitalize_first(t.name), games_path(intended_for: t.name), title: "See all games intended for #{t.name}") }.join(" // ").html_safe
+		end
+	end
+
+	def developer_type_links_list(game = @game)
+		if game && game.developer_types
+			game.developer_types.order('name ASC').collect {|t| link_to(capitalize_first(t.name), games_path(developer_type: t.name), title: "See all games by a #{t.name}") }.join(" // ").html_safe
+		end
+	end
+
+	def cost_links_list(game = @game)
+		if game && game.costs
+			game.costs.order('name ASC').collect {|t| link_to(capitalize_first(t.name), games_path(cost: t.name), title: "See all #{t.name} games") }.join(" // ").html_safe
+		end
+	end
+
 	def random_screenshot_image(game = @game)
   	if (c = game.screenshots.count) != 0
       game.screenshots.find(:first, :offset =>rand(c)).image
