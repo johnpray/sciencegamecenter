fd9444907400ed844fae69a509259c9117a48b9e
John Pray <louiegeetoo@gmail.com>
2013-03-15 12:25:53 -0400

    Route omniauth logging to standard rails logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
 	if Rails.env.production?
   	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email,user_birthday'
