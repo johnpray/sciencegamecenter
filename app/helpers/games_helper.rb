324ebfb3be64e909b36e3df4621320ca595cc894
John Pray <louiegeetoo@gmail.com>
2016-12-30 10:55:12 -0500

    Fix category link ordering

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,31 +2,31 @@ module GamesHelper
 
 	def platform_links_list(game = @game)
 		if game && game.platforms
-			game.platforms.sort_by { |name| GAME_PLATFORMS.index(name) rescue -1 }.collect {|t| link_to(t.name, games_path(platform: t.name), title: "See all games available for #{t.name}") }.join(" // ").html_safe
+			game.platforms.sort_by { |t| GAME_PLATFORMS.index(t.name) || -1 }.collect {|t| link_to(t.name, games_path(platform: t.name), title: "See all games available for #{t.name}") }.join(" // ").html_safe
 		end
 	end
 
 	def subject_links_list(game = @game)
 		if game && game.subjects
-			game.subjects.sort_by { |name| GAME_SUBJECTS.index(name) rescue -1 }.collect {|t| link_to(t.name, games_path(subject: t.name), title: "See all games that deal with #{t.name}") }.join(" // ").html_safe
+			game.subjects.sort_by { |t| GAME_SUBJECTS.index(t.name) || -1 }.collect {|t| link_to(t.name, games_path(subject: t.name), title: "See all games that deal with #{t.name}") }.join(" // ").html_safe
 		end
 	end
 
 	def intended_for_links_list(game = @game)
 		if game && game.intended_fors
-			game.intended_fors.sort_by { |name| GAME_INTENDED_FORS.index(name) rescue -1 }.collect {|t| link_to(t.name, games_path(intended_for: t.name), title: "See all games intended for #{t.name}") }.join(" // ").html_safe
+			game.intended_fors.sort_by { |t| GAME_INTENDED_FORS.index(t.name) || -1 }.collect {|t| link_to(t.name, games_path(intended_for: t.name), title: "See all games intended for #{t.name}") }.join(" // ").html_safe
 		end
 	end
 
 	def developer_type_links_list(game = @game)
 		if game && game.developer_types
-			game.developer_types.sort_by { |name| GAME_DEVELOPER_TYPES.index(name) rescue -1 }.collect {|t| link_to(t.name, games_path(developer_type: t.name), title: "See all games by a #{t.name}") }.join(" // ").html_safe
+			game.developer_types.sort_by { |t| GAME_DEVELOPER_TYPES.index(t.name) || -1 }.collect {|t| link_to(t.name, games_path(developer_type: t.name), title: "See all games by a #{t.name}") }.join(" // ").html_safe
 		end
 	end
 
 	def cost_links_list(game = @game)
 		if game && game.costs
-			game.costs.sort_by { |name| GAME_COSTS.index(name) rescue -1 }.collect {|t| link_to(t.name, games_path(cost: t.name), title: "See all #{t.name} games") }.join(" // ").html_safe
+			game.costs.sort_by { |t| GAME_COSTS.index(t.name) || -1 }.collect {|t| link_to(t.name, games_path(cost: t.name), title: "See all #{t.name} games") }.join(" // ").html_safe
 		end
 	end
 
